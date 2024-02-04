13b4e979d746171e73515b277d721336f8cd7b48
FaizanKhanDev <faizan480khan@gmail.com>
2024-02-04 06:39:38 -0800

    FIXED: AI CHATBOT

--- components/common/ChatBotIcon.js
+++ components/common/ChatBotIcon.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
+import { LoadingSpinner } from '@/components/common/LoadingSpinner'
+
 
 import axios from 'axios';
 
 
 function ChatBotIcon() {
   // Set the initial state to false the chatbox
-  const [chatVisible, setChatVisible] = useState(true);
+  const [chatVisible, setChatVisible] = useState(false);
   const [userMessage, setUserMessage] = useState('');
 
   const [chatHistory, setChatHistory] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
 
   const toggleChat = (e) => {
@@ -21,6 +24,14 @@ function ChatBotIcon() {
   }
   const handleSendMessage = async (e) => {
     e.preventDefault();
+    // Set the loading state to true
+    setIsLoading(true);
+
+    // Add the user's message to the chat history
+    setChatHistory((prevHistory) => [...prevHistory, { role: 'user', content: userMessage }]);
+
+    // Clear the user's message
+    setUserMessage('');
 
     // Make a request to OpenAI using fetch
     try {
@@ -44,15 +55,12 @@ function ChatBotIcon() {
 
       const data = await response.json();
 
-      // Add the user's message to the chat history
-      setChatHistory((prevHistory) => [...prevHistory, { role: 'user', content: userMessage }]);
 
       // Add the AI's response to the chat history
       const aiResponse = data.choices[0].message.content;
-      console.log("object", aiResponse);
       setChatHistory((prevHistory) => [...prevHistory, { role: 'ai', content: aiResponse }]);
 
-      console.log("chatHistory", chatHistory);
+      setIsLoading(false);
 
 
     } catch (error) {
@@ -128,16 +136,49 @@ function ChatBotIcon() {
                   <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
                     <div className={`rounded-full bg-gray-100 border p-1 `}>
                       <svg stroke="none" fill="black" strokeWidth="1.5" viewBox="0 0 24 24" aria-hidden="true" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
-                        <path strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"></path>
+                        <path strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 
+                        4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 
+                        2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 
+                        3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"></path>
                       </svg>
                     </div>
                   </span>
                   <p className={`leading-relaxed text-gray-700 ${message.role == 'ai' ? '' : ''}`}>
                     <span className="block font-bold text-gray-700">{message.role == 'ai' ? 'AI' : 'You'}</span>
                     {message.content}
+
                   </p>
                 </div>
               ))}
+              {isLoading &&
+                <>
+                  <div className='flex gap-3 my-4 text-gray-600 text-sm'>
+
+                    <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
+                      <div className={`rounded-full bg-gray-100 border p-1 `}>
+                        <svg stroke="none" fill="black" strokeWidth="1.5" viewBox="0 0 24 24" aria-hidden="true" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+                          <path strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"></path>
+                        </svg>
+                      </div>
+                    </span>
+                    <p className={`leading-relaxed text-gray-700`}><span className="block font-bold text-gray-700">AI</span></p>
+                  </div>
+                  <span className="animate-spin text-gray-500 ml-2">
+                    <main
+                      style={{ marginLeft: '65px', marginTop: '-40px' }}
+                    >
+                      <div className="col-3">
+                        <div className="snippet" data-title="dot-pulse">
+                          <div className="stage">
+                            <div className="dot-pulse"></div>
+                          </div>
+                        </div>
+                      </div>
+                    </main>
+                  </span>
+
+                </>
+              }
             </div>
             <div className="flex items-center pt-0">
 
@@ -155,14 +196,15 @@ function ChatBotIcon() {
                       text-[#030712] focus-visible:ring-offset-2"
                 />
                 <button
-                  className="inline-flex items-center justify-center
-                  rounded-md text-sm 
-                  font-medium text-[#f9fafb]
-                  bg-black hover:bg-[#111827E6] h-10 px-4 py-2"
-                  disabled={!userMessage.trim()} // Disable if userMessage is empty or only contains whitespace
+                  className={`inline-flex items-center justify-center rounded-md text-sm font-medium ${!userMessage.trim() || isLoading
+                      ? 'text-[#fff] opacity-50 bg-black cursor-not-allowed'
+                      : 'text-[#f9fafb] bg-black hover:bg-[#111827E6]'
+                    } h-10 px-4 py-2`}
+                  disabled={!userMessage.trim() || isLoading}
                 >
                   Send
                 </button>
+
               </form>
             </div>
           </div>
